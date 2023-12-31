91be99ed670660954e66bb0150e445047e91f6e0
Muhammad235 <adelekeyahaya05@gmail.com>
2023-12-31 14:37:13 +0000

    Edit: edit the default breeze login page to custom login template with ability for form submission

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,89 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('web.layouts.master')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+@section('content')
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <!--=============================
+        BREADCRUMB START
+    ==============================-->
+    <section class="fp__breadcrumb" style="background: url(images/counter_bg.jpg);">
+        <div class="fp__breadcrumb_overlay">
+            <div class="container">
+                <div class="fp__breadcrumb_text">
+                    <h1>sign in</h1>
+                    <ul>
+                        <li><a href="index.html">home</a></li>
+                        <li><a href="#">sign in</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+    </section>
+    <!--=============================
+        BREADCRUMB END
+    ==============================-->
+    
+    <!--=========================
+        SIGNIN START
+    ==========================-->
+    <section class="fp__signin" style="background: url(images/login_bg.jpg);">
+        <div class="fp__signin_overlay pt_125 xs_pt_95 pb_100 xs_pb_70">
+            <div class="container">
+                <div class="row wow fadeInUp" data-wow-duration="1s">
+                    <div class="col-xxl-5 col-xl-6 col-md-9 col-lg-7 m-auto">
+                        <div class="fp__login_area">
+                            <h2>Welcome back!</h2>
+                            <p>sign in to continue</p>
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label>email</label>
+                                            <input type="email" name="email" placeholder="Email" required >
+                                        </div>
+                                    </div>
+                                    <div class="required autofocuscol-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label>password</label>
+                                            <input type="password" name="password" placeholder="Password" required autofocus>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput fp__login_check_area">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value=""
+                                                    id="flexCheckDefault" name="remember">
+                                                <label class="form-check-label" for="flexCheckDefault">
+                                                    Remeber Me
+                                                </label>
+                                            </div>
+                                            <a href="forgot_password.html">Forgot Password ?</a>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <button type="submit" class="common_btn">login</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <p class="or"><span>or</span></p>
+                            <ul class="d-flex">
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                            </ul>
+                            <p class="create_account">Dont’t have an aceount ? <a href="sign_up.html">Create Account</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </section>
+    <!--=========================
+        SIGNIN END
+    ==========================-->
+@endsection
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
