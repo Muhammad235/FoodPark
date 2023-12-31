869411b0af3ed3224841976900e3ccf863261a0a
Muhammad235 <adelekeyahaya05@gmail.com>
2023-12-31 19:19:42 +0000

    Create: sign up view page with submission ability

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +49,91 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+@extends('web.layouts.master')
+@section('content')
+        <!--=============================
+        BREADCRUMB START
+    ==============================-->
+    <section class="fp__breadcrumb" style="background: url(images/counter_bg.jpg);">
+        <div class="fp__breadcrumb_overlay">
+            <div class="container">
+                <div class="fp__breadcrumb_text">
+                    <h1>sign up</h1>
+                    <ul>
+                        <li><a href="index.html">home</a></li>
+                        <li><a href="#">sign up</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--=============================
+        BREADCRUMB END
+    ==============================-->
+
+
+    <!--=========================
+        SIGN UP START
+    ==========================-->
+    <section class="fp__signup" style="background: url(images/login_bg.jpg);">
+        <div class="fp__signup_overlay pt_125 xs_pt_95 pb_100 xs_pb_70">
+            <div class=" container">
+                <div class="row wow fadeInUp" data-wow-duration="1s">
+                    <div class="col-xxl-5 col-xl-6 col-md-9 col-lg-7 m-auto">
+                        <div class="fp__login_area">
+                            <h2>Welcome back!</h2>
+                            <p>sign up to continue</p>
+                            <form>
+                                <div class="row">
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label>name</label>
+                                            <input type="text" placeholder="Name">
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label>email</label>
+                                            <input type="email" placeholder="Email">
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label>password</label>
+                                            <input type="password" placeholder="Password">
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label>confirm password</label>
+                                            <input type="password" placeholder="Confirm Password">
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <button type="submit" class="common_btn">login</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <p class="or"><span>or</span></p>
+                            <ul class="d-flex">
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                            </ul>
+                            <p class="create_account">Dont’t have an aceount ? <a href="sign_in.html">login</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--=========================
+        SIGN UP END
+    ==========================-->
+@endsection
