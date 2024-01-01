64c058afb8797cbe2b85e68d5ee550042f36b06c
Muhammad235 <adelekeyahaya05@gmail.com>
2024-01-01 20:14:16 +0000

    Add: Admin credentials settings card to Admin Profile

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -3,10 +3,58 @@
 @section('content')
 <section class="section">
     <div class="section-header">
-      <h1>Profile</h1>
+      <h1>Update User</h1>
     </div>
 
     <div class="section-body">
+        <div class="card card-primary">
+            <div class="card-header">
+              <h4>Update User settings</h4>
+            </div>
+            <div class="card-body">
+              <form action="">
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" class="form-control" name="name" value="{{ auth()->user()->name }}">
+                </div>
+
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" class="form-control" name="email" value="{{ auth()->user()->email }}">
+                </div>
+                  <button class="btn btn-primary" type="submit">Save</button>
+              </form>
+            </div>
+        </div>
+
+        <div class="card card-primary">
+            <div class="card-header">
+              <h4>Update Password</h4>
+            </div>
+            <div class="card-body">
+              <form action="">
+                <div class="form-group">
+                    <label>Current Password</label>
+                    <input type="password" class="form-control" name="password">
+                </div>
+
+                <div class="form-group">
+                    <label>New Password</label>
+                    <input type="password" class="form-control" name="newpassword">
+                </div>
+
+                <div class="form-group">
+                    <label>Confirm Password</label>
+                    <input type="email" class="form-control" name="password">
+                </div>
+
+                <button class="btn btn-primary" type="submit">Save</button>
+              </form>
+            </div>
+        </div>
     </div>
-</section>
+
+
+
+  </section>
 @endsection
