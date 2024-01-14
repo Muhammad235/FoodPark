e2af73322b1bbbad1fb56646741f1194157f856b
Muhammad235 <adelekeyahaya05@gmail.com>
2024-01-14 23:00:24 +0000

    Feat: WhyChooseUs Section Display

--- app/Http/Controllers/Web/WebPageController.php
+++ app/Http/Controllers/Web/WebPageController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Web;
 
 use App\Models\Slider;
+use App\Models\WhyChooseUs;
+use App\Models\SectionTitle;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\Contracts\View\View;
 use App\Http\Controllers\Controller;
 
@@ -12,6 +15,16 @@ class WebPageController extends Controller
     public function index() : View {
         
         $sliders = Slider::where('status', 1)->get();
-        return view('web.home.index', compact('sliders'));
+        $sectionTitles = $this->getSectionTitle();
+        $WhyChooseUs = WhyChooseUs::where('status', 1)->get();
+
+        return view('web.home.index', compact('sliders', 'sectionTitles', 'WhyChooseUs'));
+    }
+
+    public function getSectionTitle() : Collection {
+        $keys = ['why_choose_top_title', 'why_choose_main_title', 'why_choose_sub_title'];
+        $titles = SectionTitle::whereIn('key', $keys)->pluck('value', 'key');
+
+        return $titles;
     }
 }
