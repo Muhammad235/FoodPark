e879401eb3ea7de13394baf0bb40845f6ea9ef0e
Muhammad235 <adelekeyahaya05@gmail.com>
2024-02-04 22:58:13 +0000

    Feat: Dynamic display of related product on menu details page

--- resources/views/web/pages/menu_details.blade.php
+++ resources/views/web/pages/menu_details.blade.php
@@ -1,7 +1,6 @@
 @extends('web.layouts.master')
 @section('content')
 
-@dump($relatedProducts)
     <!--=============================
         BREADCRUMB START
     ==============================-->
@@ -253,136 +252,43 @@
             <div class="fp__related_menu mt_90 xs_mt_60">
                 <h2>related item</h2>
                 <div class="row related_product_slider">
-                    <div class="col-xl-3 wow fadeInUp" data-wow-duration="1s">
-                        <div class="fp__menu_item">
-                            <div class="fp__menu_item_img">
-                                <img src="images/menu2_img_1.jpg" alt="menu" class="img-fluid w-100">
-                                <a class="category" href="#">chicken</a>
+                    @if (count($relatedProducts) > 0)
+                       @foreach ($relatedProducts as $relatedProduct)
+                        <div class="col-xl-3 wow fadeInUp" data-wow-duration="1s">
+                            <div class="fp__menu_item">
+                                <div class="fp__menu_item_img">
+                                    <img src="{{ asset($relatedProduct->thumb_image) }}" alt="{{ $relatedProduct->name }}" class="img-fluid w-100">
+                                    <a class="category" href="javascript:;">{{ $relatedProduct->category->name }}</a>
+                                </div>
+                                <div class="fp__menu_item_text">
+                                    <p class="rating">
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star-half-alt"></i>
+                                        <i class="far fa-star"></i>
+                                        <span>74</span>
+                                    </p>
+                                    <a class="title" href="menu_details.html">{{ $relatedProduct->name }}</a>
+                                    <h5 class="price">
+                                        @if($relatedProduct->offer_price > 0) 
+                                           ${{$relatedProduct->offer_price }}
+                                           <del>${{$relatedProduct->price}}</del> 
+                                        @else
+                                            ${{$relatedProduct->offer_price }}
+                                        @endif
+                                    </h5>
+                                    <ul class="d-flex flex-wrap justify-content-center">
+                                        <li><a href="#" data-bs-toggle="modal" data-bs-target="#cartModal"><i
+                                                    class="fas fa-shopping-basket"></i></a></li>
+                                        <li><a href="#"><i class="fal fa-heart"></i></a></li>
+                                        <li><a href="#"><i class="far fa-eye"></i></a></li>
+                                    </ul>
+                                </div>
                             </div>
-                            <div class="fp__menu_item_text">
-                                <p class="rating">
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star-half-alt"></i>
-                                    <i class="far fa-star"></i>
-                                    <span>74</span>
-                                </p>
-                                <a class="title" href="menu_details.html">chicken Masala</a>
-                                <h5 class="price">$80.00 <del>90.00</del></h5>
-                                <ul class="d-flex flex-wrap justify-content-center">
-                                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#cartModal"><i
-                                                class="fas fa-shopping-basket"></i></a></li>
-                                    <li><a href="#"><i class="fal fa-heart"></i></a></li>
-                                    <li><a href="#"><i class="far fa-eye"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 wow fadeInUp" data-wow-duration="1s">
-                        <div class="fp__menu_item">
-                            <div class="fp__menu_item_img">
-                                <img src="images/menu2_img_2.jpg" alt="menu" class="img-fluid w-100">
-                                <a class="category" href="#">chicken</a>
-                            </div>
-                            <div class="fp__menu_item_text">
-                                <p class="rating">
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star-half-alt"></i>
-                                    <i class="far fa-star"></i>
-                                    <span>120</span>
-                                </p>
-                                <a class="title" href="menu_details.html">chicken Masala</a>
-                                <h5 class="price">$80.00 <del>90.00</del></h5>
-                                <ul class="d-flex flex-wrap justify-content-center">
-                                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#cartModal"><i
-                                                class="fas fa-shopping-basket"></i></a></li>
-                                    <li><a href="#"><i class="fal fa-heart"></i></a></li>
-                                    <li><a href="#"><i class="far fa-eye"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 wow fadeInUp" data-wow-duration="1s">
-                        <div class="fp__menu_item">
-                            <div class="fp__menu_item_img">
-                                <img src="images/menu2_img_3.jpg" alt="menu" class="img-fluid w-100">
-                                <a class="category" href="#">Biryani</a>
-                            </div>
-                            <div class="fp__menu_item_text">
-                                <p class="rating">
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star-half-alt"></i>
-                                    <i class="far fa-star"></i>
-                                    <span>514</span>
-                                </p>
-                                <a class="title" href="menu_details.html">Hyderabadi biryani</a>
-                                <h5 class="price">$70.00</h5>
-                                <ul class="d-flex flex-wrap justify-content-center">
-                                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#cartModal"><i
-                                                class="fas fa-shopping-basket"></i></a></li>
-                                    <li><a href="#"><i class="fal fa-heart"></i></a></li>
-                                    <li><a href="#"><i class="far fa-eye"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 wow fadeInUp" data-wow-duration="1s">
-                        <div class="fp__menu_item">
-                            <div class="fp__menu_item_img">
-                                <img src="images/menu2_img_4.jpg" alt="menu" class="img-fluid w-100">
-                                <a class="category" href="#">grill</a>
-                            </div>
-                            <div class="fp__menu_item_text">
-                                <p class="rating">
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star-half-alt"></i>
-                                    <i class="far fa-star"></i>
-                                    <span>25</span>
-                                </p>
-                                <a class="title" href="menu_details.html">daria shevtsova</a>
-                                <h5 class="price">$99.00</h5>
-                                <ul class="d-flex flex-wrap justify-content-center">
-                                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#cartModal"><i
-                                                class="fas fa-shopping-basket"></i></a></li>
-                                    <li><a href="#"><i class="fal fa-heart"></i></a></li>
-                                    <li><a href="#"><i class="far fa-eye"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 wow fadeInUp" data-wow-duration="1s">
-                        <div class="fp__menu_item">
-                            <div class="fp__menu_item_img">
-                                <img src="images/menu2_img_5.jpg" alt=" menu" class="img-fluid w-100">
-                                <a class="category" href="#">chicken</a>
-                            </div>
-                            <div class="fp__menu_item_text">
-                                <p class="rating">
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star"></i>
-                                    <i class="fas fa-star-half-alt"></i>
-                                    <i class="far fa-star"></i>
-                                    <span>324</span>
-                                </p>
-                                <a class="title" href="menu_details.html">chicken Masala</a>
-                                <h5 class="price">$80.00 <del>90.00</del></h5>
-                                <ul class="d-flex flex-wrap justify-content-center">
-                                    <li><a href="#" data-bs-toggle="modal" data-bs-target="#cartModal"><i
-                                                class="fas fa-shopping-basket"></i></a></li>
-                                    <li><a href="#"><i class="fal fa-heart"></i></a></li>
-                                    <li><a href="#"><i class="far fa-eye"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                         </div>
+                       @endforeach 
+                    @endif
                 </div>
             </div>
         </div>
